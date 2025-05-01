6569a176adfa2be63368571d10564bb60e5b4f5e
luangomes <luangomespctu@gmail.com>
2025-05-01 16:44:18 -0300

    feat: add Ribbon Load Balancer

--- hr-worker/src/main/java/com/microservices/project/hrworker/controller/WorkerResource.java
+++ hr-worker/src/main/java/com/microservices/project/hrworker/controller/WorkerResource.java
@@ -1,8 +1,11 @@
 package com.microservices.project.hrworker.controller;
 
+import ch.qos.logback.classic.Logger;
 import com.microservices.project.hrworker.dto.WorkerDTO;
 import com.microservices.project.hrworker.service.WorkerService;
 import com.microservices.project.hrworker.service.exceptions.ResourceNotFoundException;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +20,16 @@ public class WorkerResource {
 
 
     private final WorkerService workerService;
+    private final Environment environment;
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(WorkerResource.class);
 
-    public WorkerResource(WorkerService workerService) {
+    public WorkerResource(WorkerService workerService, Environment environment) {
         this.workerService = workerService;
+        this.environment = environment;
     }
 
     @GetMapping
-    public ResponseEntity<List<WorkerDTO>> findAllWorkers () {
+    public ResponseEntity<List<WorkerDTO>> findAllWorkers() {
 
         List<WorkerDTO> allWorkers = workerService.findAllWorkers();
 
@@ -31,14 +37,15 @@ public class WorkerResource {
 
     }
 
-    @GetMapping (value = "/{id}")
-    public ResponseEntity<WorkerDTO> findWorkerById (@PathVariable (name = "id") Long id) throws ResourceNotFoundException {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<WorkerDTO> findWorkerById(@PathVariable(name = "id") Long id) throws ResourceNotFoundException {
+
+        logger.warn("PORT=" + environment.getProperty("local.server.port"));
 
         WorkerDTO worker = workerService.findWorkerByID(id);
 
         return ResponseEntity.ok(worker);
     }
-
 
 
 }
