f60d334b0377a4badbab662cf0c0a034b3f510af
luangomes <luangomespctu@gmail.com>
2025-05-04 09:38:42 -0300

    feat: Hystrix fault tolerance implementation

--- hrpayroll/src/main/java/com/microservices/project/hrpayroll/controller/PaymentResource.java
+++ hrpayroll/src/main/java/com/microservices/project/hrpayroll/controller/PaymentResource.java
@@ -3,6 +3,7 @@ package com.microservices.project.hrpayroll.controller;
 import com.microservices.project.hrpayroll.entity.Payment;
 import com.microservices.project.hrpayroll.service.PaymentService;
 import com.microservices.project.hrpayroll.service.exceptions.ResourceNotFoundException;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/api/payment")
+@RequestMapping("/api/payment")
 public class PaymentResource {
     private final PaymentService paymentService;
 
@@ -19,10 +20,20 @@ public class PaymentResource {
     }
 
 
-    @GetMapping ("/{workerId}/days/{workedDays}")
-    public ResponseEntity<Payment> getWorkerPayment (@PathVariable (name = "workerId") Long workerId, @PathVariable (name = "workedDays") Integer workedDays) throws ResourceNotFoundException {
+    @HystrixCommand(fallbackMethod = "getWorkerPaymentAlternative")
+    @GetMapping("/{workerId}/days/{workedDays}")
+    public ResponseEntity<Payment> getWorkerPayment(@PathVariable(name = "workerId") Long workerId, @PathVariable(name = "workedDays") Integer workedDays) throws ResourceNotFoundException {
 
         Payment payment = paymentService.getPayment(workerId, workedDays);
+
+        return ResponseEntity.ok(payment);
+
+    }
+
+
+    public ResponseEntity<Payment> getWorkerPaymentAlternative(Long workerId, Integer workedDays) {
+
+        Payment payment = new Payment("Brann", 400.0, workedDays);
 
         return ResponseEntity.ok(payment);
 
