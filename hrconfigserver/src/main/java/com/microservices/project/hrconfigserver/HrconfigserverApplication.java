22ec67e58306863864612124cf9f3e2ae40b690d
luangomes <luangomespctu@gmail.com>
2025-05-07 19:36:10 -0300

    feat: add configuration server

---
+++ hrconfigserver/src/main/java/com/microservices/project/hrconfigserver/HrconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.project.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrconfigserverApplication.class, args);
+	}
+
+}
---
+++ hrconfigserver/src/test/java/com/microservices/project/hrconfigserver/HrconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.project.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
