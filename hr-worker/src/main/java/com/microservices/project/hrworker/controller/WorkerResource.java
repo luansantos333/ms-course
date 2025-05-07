5108f545f1d3d1662672e09c20dd004ffa004e2b
luangomes <luangomespctu@gmail.com>
2025-05-07 20:10:43 -0300

    feat: add bootstrap.properties file to hr-worker

--- hr-worker/src/main/java/com/microservices/project/hrworker/controller/WorkerResource.java
+++ hr-worker/src/main/java/com/microservices/project/hrworker/controller/WorkerResource.java
@@ -5,6 +5,7 @@ import com.microservices.project.hrworker.dto.WorkerDTO;
 import com.microservices.project.hrworker.service.WorkerService;
 import com.microservices.project.hrworker.service.exceptions.ResourceNotFoundException;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +21,23 @@ public class WorkerResource {
 
 
     private final WorkerService workerService;
+    @Value("${test.config}")
+    private String testConfig;
 
 
     public WorkerResource(WorkerService workerService) {
         this.workerService = workerService;
+    }
+
+
+
+    @GetMapping ("/configs")
+    public ResponseEntity<Void> getAllConfigs () {
+
+        System.out.println("CONFIG=" + testConfig);
+
+        return ResponseEntity.noContent().build();
+
     }
 
     @GetMapping
