f305782f1901ee32bbc40f8ec24c8187ee579ce8
luangomes <luangomespctu@gmail.com>
2025-05-18 10:39:33 -0300

    feat: add cors filter config

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,6 +1,10 @@
 package com.devsuperior.hrapigatewayzuul.config;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -8,6 +12,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -35,5 +44,35 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
                 .antMatchers(HttpMethod.POST, OPERATOR).hasRole("ADMIN").antMatchers(ADMIN).hasRole("ADMIN")
                 .anyRequest().authenticated();
+
+        http.cors().configurationSource(corsConfigurationSource());
     }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource () {
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*")); // libera o acesso para qualquer origem (podem ser especificados uma lista de domínios)
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+
+    }
+
+/*
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter () {
+
+        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(new CorsFilter (corsConfigurationSource()));
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return filter;
+    }
+*/
+
 }
